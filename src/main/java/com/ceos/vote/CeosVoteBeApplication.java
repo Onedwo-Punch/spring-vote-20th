7117e0d33cadd4735d5f72878235b08d4ed89074
limgahyun <limgh2002@gmail.com>
2024-12-01 00:43:31 +0900

    git init

---
+++ src/main/java/com/ceos/vote/CeosVoteBeApplication.java
@@ -0,0 +1,13 @@
+package com.ceos.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeosVoteBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeosVoteBeApplication.class, args);
+    }
+
+}
