34c5b5a4c4d4d777d181bc669f10c7faa0c4e513
limgahyun <limgh2002@gmail.com>
2024-12-01 01:08:01 +0900

    feat: swagger setting

---
+++ src/main/java/com/ceos/vote/global/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.ceos.vote.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API Test",
+                description = "CEOS vote service API 명세서",
+                version = "v1"
+        ),
+        servers = {@Server(url = "http://localhost:8080", description = "local server")}
+)
+
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components());
+    }
+}
