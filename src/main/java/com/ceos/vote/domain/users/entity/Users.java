ba6a96d504390bf17ccdae2cfadd0a118271f808
limgahyun <limgh2002@gmail.com>
2024-12-01 01:04:12 +0900

    feat: users entity, repository

---
+++ src/main/java/com/ceos/vote/domain/users/entity/Users.java
@@ -0,0 +1,33 @@
+package com.ceos.vote.domain.users.entity;
+
+import com.ceos.vote.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Users extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(length = 32)
+    private String username;
+
+    @Column
+    private String email;
+
+    @Builder
+    public Users(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+}
