86627e6d817b1ac8b20e7a29cd6930d9fccdd0c7
limgahyun <limgh2002@gmail.com>
2025-01-06 18:45:26 +0900

    test

--- src/main/java/com/ceos/vote/CeosVoteBeApplication.java
+++ src/main/java/com/ceos/vote/CeosVoteBeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CeosVoteBeApplication {
 
+    //test
     public static void main(String[] args) {
         SpringApplication.run(CeosVoteBeApplication.class, args);
     }
